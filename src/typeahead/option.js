2823a1de6aa79eee49593801d9c599b64900b945
Egor Voronov <egorvoronov@gmail.com>
2017-07-02 19:02:12 -0500

    [FIXED] checking for isArray first

--- src/typeahead/option.js
+++ src/typeahead/option.js
@@ -7,65 +7,73 @@ var classNames = require('classnames');
  * A single option within the TypeaheadSelector
  */
 var TypeaheadOption = createClass({
-  propTypes: {
-    customClasses: PropTypes.object,
-    customValue: PropTypes.string,
-    onClick: PropTypes.func,
-    children: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
-    hover: PropTypes.bool
-  },
+    propTypes: {
+        customClasses: PropTypes.object,
+        customValue: PropTypes.string,
+        onClick: PropTypes.func,
+        children: PropTypes.oneOfType([PropTypes.string, PropTypes.array]),
+        hover: PropTypes.bool
+    },
 
-  getDefaultProps: function() {
-    return {
-      customClasses: {},
-      onClick: function(event) {
+    getDefaultProps: function () {
+        return {
+            customClasses: {},
+            onClick: function (event) {
+                event.preventDefault();
+            }
+        };
+    },
+
+    render: function () {
+        var classes = {};
+        classes[this.props.customClasses.hover || "hover"] = !!this.props.hover;
+        classes[this.props.customClasses.listItem] = !!this.props.customClasses.listItem;
+
+        if (this.props.customValue) {
+            classes[this.props.customClasses.customAdd] = !!this.props.customClasses.customAdd;
+        }
+
+        var classList = classNames(classes);
+
+        var title, customOptionChild;
+        if (Array.isArray(this.props.children)) {
+            title = this.props.children[0];
+            customOptionChild = this.props.children[1];
+        } else {
+            title = this.props.children;
+        }
+
+        return (
+            <li className={classList} onClick={this._onClick}>
+                <a href="javascript: void 0;" className={this._getClasses()} ref="anchor">
+                    { title }
+                </a>
+                { customOptionChild && (
+                    <div className="custom-option-child" onClick={this._onCustomOptionChildClick}>
+                        { customOptionChild }
+                    </div>
+                )}
+            </li>
+        );
+    },
+
+    _onCustomOptionChildClick(e) {
+        this.props.onCustomOptionChildClick(e);
+    },
+
+    _getClasses: function () {
+        var classes = {
+            "typeahead-option": true,
+        };
+        classes[this.props.customClasses.listAnchor] = !!this.props.customClasses.listAnchor;
+
+        return classNames(classes);
+    },
+
+    _onClick: function (event) {
         event.preventDefault();
-      }
-    };
-  },
-
-  render: function() {
-    var classes = {};
-    classes[this.props.customClasses.hover || "hover"] = !!this.props.hover;
-    classes[this.props.customClasses.listItem] = !!this.props.customClasses.listItem;
-
-    if (this.props.customValue) {
-      classes[this.props.customClasses.customAdd] = !!this.props.customClasses.customAdd;
+        return this.props.onClick(event);
     }
-
-    var classList = classNames(classes);
-
-    return (
-      <li className={classList} onClick={this._onClick}>
-        <a href="javascript: void 0;" className={this._getClasses()} ref="anchor">
-          { this.props.children[0] }
-        </a>
-        { this.props.children[1] && (
-          <div className="custom-option-child" onClick={this._onCustomOptionChildClick}>
-            { this.props.children[ 1 ] }
-          </div>
-        )}
-      </li>
-    );
-  },
-
-  _onCustomOptionChildClick(e) {
-    this.props.onCustomOptionChildClick(e);
-  },
-
-  _getClasses: function() {
-    var classes = {
-      "typeahead-option": true,
-    };
-    classes[this.props.customClasses.listAnchor] = !!this.props.customClasses.listAnchor;
-
-    return classNames(classes);
-  },
-
-  _onClick: function(event) {
-    event.preventDefault();
-    return this.props.onClick(event);
-  }
 });
 
 
